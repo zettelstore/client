6e0853b04266c48f335bc170ad981c513424073a
stern <>
2022-01-27 18:06:38 +0000

    New constant value for syntax "svg"
    FossilOrigin-Name: 3679918e29943ab5e0ff9ea5d9711a353f3d0950f4a60f0ef6c7481c62bb9cb8

--- api/const.go
+++ api/const.go
@@ -98,6 +98,7 @@ const (
 	ValueSyntaxDraw        = "draw"
 	ValueSyntaxGif         = "gif"
 	ValueSyntaxNone        = "none"
+	ValueSyntaxSVG         = "svg"
 	ValueSyntaxText        = "text"
 	ValueSyntaxZmk         = "zmk"
 	ValueFalse             = "false"
