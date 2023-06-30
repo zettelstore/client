de0648b804e165e77c48bcf256d98a4d7d86e4bb
stern <>
2023-06-30 18:11:50 +0000

    Add package sx for helper to work with sxpf data
    FossilOrigin-Name: 1bb202cfb8cbd79066b843257a6dcfdfcef995d680463ec5e6b1615d19e54202

---
+++ sx/sx.go
@@ -0,0 +1,76 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2023-present Detlef Stern
+//
+// This file is part of zettelstore-client.
+//
+// Zettelstore client is licensed under the latest version of the EUPL
+// (European Union Public License). Please see file LICENSE.txt for your rights
+// and obligations under this license.
+//-----------------------------------------------------------------------------
+
+// Package sx contains helper function to work with s-expression in an alien environment.
+package sx
+
+import (
+	"errors"
+	"fmt"
+
+	"codeberg.org/t73fde/sxpf"
+)
+
+// ParseObject parses the given object as a proper list, based on a type specification.
+func ParseObject(obj sxpf.Object, spec string) ([]sxpf.Object, error) {
+	cell, isCell := sxpf.GetCell(obj)
+	if !isCell {
+		return nil, fmt.Errorf("not a list: %T/%v", obj, obj)
+	}
+	if cell == nil {
+		if spec == "" {
+			return nil, nil
+		}
+		return nil, ErrElementsMissing
+	}
+
+	result := make([]sxpf.Object, 0, len(spec))
+	node, i := cell, 0
+	for ; node != nil; i++ {
+		if i >= len(spec) {
+			return nil, ErrNoSpec
+		}
+		var val sxpf.Object
+		var ok bool
+		car := node.Car()
+		switch spec[i] {
+		case 'b':
+			val, ok = sxpf.GetBoolean(car)
+		case 'c':
+			val, ok = sxpf.GetCell(car)
+		case 'i':
+			val, ok = car.(sxpf.Int64)
+		case 'o':
+			val, ok = car, true
+		case 's':
+			val, ok = sxpf.GetString(car)
+		case 'y':
+			val, ok = sxpf.GetSymbol(car)
+		default:
+			return nil, fmt.Errorf("unknown spec '%c", spec[i])
+		}
+		if !ok {
+			return nil, fmt.Errorf("does not match spec '%v': %v", spec[i], car)
+		}
+		result = append(result, val)
+		next, isNextCell := sxpf.GetCell(node.Cdr())
+		if !isNextCell {
+			return nil, sxpf.ErrImproper{Cell: cell}
+		}
+		node = next
+	}
+	if i < len(spec) {
+		return nil, ErrElementsMissing
+	}
+	return result, nil
+}
+
+var ErrElementsMissing = errors.New("spec contains more data")
+var ErrNoSpec = errors.New("no spec for list elements")
---
+++ sx/sx_test.go
@@ -0,0 +1,54 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2023-present Detlef Stern
+//
+// This file is part of zettelstore-client.
+//
+// Zettelstore client is licensed under the latest version of the EUPL
+// (European Union Public License). Please see file LICENSE.txt for your rights
+// and obligations under this license.
+//-----------------------------------------------------------------------------
+
+package sx_test
+
+import (
+	"testing"
+
+	"codeberg.org/t73fde/sxpf"
+	"zettelstore.de/c/sx"
+)
+
+func TestParseObject(t *testing.T) {
+	if elems, err := sx.ParseObject(sxpf.MakeString("a"), "s"); err == nil {
+		t.Error("expected an error, but got: ", elems)
+	}
+	if elems, err := sx.ParseObject(sxpf.Nil(), ""); err != nil {
+		t.Error(err)
+	} else if len(elems) != 0 {
+		t.Error("Must be empty, but got:", elems)
+	}
+	if elems, err := sx.ParseObject(sxpf.Nil(), "b"); err == nil {
+		t.Error("expected error, but got: ", elems)
+	}
+
+	if elems, err := sx.ParseObject(sxpf.MakeList(sxpf.MakeString("a")), "ss"); err == nil {
+		t.Error("expected error, but got: ", elems)
+	}
+	if elems, err := sx.ParseObject(sxpf.MakeList(sxpf.MakeString("a")), ""); err == nil {
+		t.Error("expected error, but got: ", elems)
+	}
+	if elems, err := sx.ParseObject(sxpf.MakeList(sxpf.MakeString("a")), "b"); err == nil {
+		t.Error("expected error, but got: ", elems)
+	}
+	if elems, err := sx.ParseObject(sxpf.Cons(sxpf.Nil(), sxpf.MakeString("a")), "cs"); err == nil {
+		t.Error("expected error, but got: ", elems)
+	}
+
+	if elems, err := sx.ParseObject(sxpf.MakeList(sxpf.MakeString("a")), "s"); err != nil {
+		t.Error(err)
+	} else if len(elems) != 1 {
+		t.Error("length == 1, but got: ", elems)
+	} else {
+		_ = elems[0].(sxpf.String)
+	}
+
+}
